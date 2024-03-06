3ec2f1e425ebed00bc9c179cf6500e8cc583ffc3
sidhart25 <navneetnishchal1420@gmail.com>
2024-03-06 23:47:30 +0530

    cut them all updated

--- trees/it-restaurants.cpp
+++ trees/it-restaurants.cpp
@@ -4,24 +4,27 @@ using namespace std;
 #define ll long long
 #define mod LLONG_MAX
 
-int dfs(vector<vector<int>>& tree,int curr,vector<bool>&vis,vector<int>&dp,int& count){
+int dfs(vector<vector<int>>&tree,int curr,vector<bool>&vis,map<int,int>&mp,int n){
     if(vis[curr]){
         return 0;
     }
+    vis[curr]=true;
     if(tree[curr].size()==1 and curr != 1){
-        dp[curr]=1;
         return 1;
     }
-    vis[curr]=true;
-    int c=0;
+    int count=0;
     for(int i=0;i<tree[curr].size();i++){
-        c=c+dfs(tree,tree[curr][i],vis,dp,count);
+        int child=tree[curr][i];
+        int c=dfs(tree,child,vis,mp);
+        count=count+c;
     }
-   c=c+1;
-   if(c %2==0){
-       count=count+1;
-   }
-    return c;
+    int rem=n-1-count;
+    if(rem != 0 and count != 0){
+        mp[rem]=count;
+        mp[count]=rem;
+    }
+    return count+1;
+
 }
 
 void solve(){
@@ -34,23 +37,18 @@ void solve(){
         tree[u].push_back(v);
         tree[v].push_back(u);
     }
-    if(n%2==1){
-        cout<<"-1"<<endl;
-        return;
-    }
-    vector<int>dp(n+1);
+    map<int,int>mp;
     vector<bool>vis(n+1,false);
     int node=1;
-    int count=0;
-    int c=dfs(tree,node,vis,dp,count);
-    if(count==0){
-        cout<<"-1"<<endl;
-        return;
+    int ans=dfs(tree,node,vis,mp,n);
+    cout<<mp.size()<<endl;
+    for(auto it:mp){
+        int f=it.first;
+        int s=it.second;
+        cout<<f<<" "<,s<<endl;
     }
-    cout<<count-1<<endl;
-    return;
-   
-    }
+
+}
     
 
 
@@ -58,9 +56,9 @@ int main() {
     ios_base::sync_with_stdio(false);
     cin.tie(NULL);
 
-    ll t=1;
+    ll t;
     
-    //cin>>t;
+    cin>>t;
     while(t--){
         solve();
      }
