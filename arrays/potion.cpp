0755a7b5306ad566c3709f6a453f81d3bba2f3d1
sidhart25 <navneetnishchal1420@gmail.com>
2024-03-11 13:31:45 +0530

    potion easy and hard version problem solved

--- arrays/potion.cpp
+++ arrays/potion.cpp
@@ -25,22 +25,29 @@ ll dfs(vector<vector<ll>>& tree,ll curr,vector<bool>&vis,vector<ll>&dp,ll& count
 }
  
 void solve(){
-    int n;
+    ll n;
     cin>>n;
-    vector<int>arr(n);
-    for(int i=0;i<n;i++){
+    vector<ll>arr(n);
+    for(ll i=0;i<n;i++){
         cin>>arr[i];
     }
-    int sum=0;
-    int count=0;
-    for(int i=0;i<n;i++){
-        s=s+arr[i];
-        if(s<0){
-            
+    ll sum=0;
+    ll count=0;
+    priority_queue<ll, vector<ll>, greater<ll>> pq;
+    for(ll i=0;i<n;i++){
+        sum=sum+arr[i];
+        count++;
+        if(arr[i]<0){
+            pq.push(arr[i]);
         }
+        if(sum<0){
+            sum=sum-pq.top();
+            pq.pop();
+            count--;
+        }
+
     }
-    
-  
+    cout<<count<<endl;
 
  
    
@@ -51,9 +58,9 @@ int main() {
     ios_base::sync_with_stdio(false);
     cin.tie(NULL);
  
-    ll t;
+    ll t=1;
     
-    cin>>t;
+    //cin>>t;
     while(t--){
        // cout<<"t "<<t<<endl;
         solve();
