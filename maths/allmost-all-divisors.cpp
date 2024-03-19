3eeda90f2be9b65cbcbec4ef5749c8881de3e217
sidhart25 <navneetnishchal1420@gmail.com>
2024-03-20 02:24:37 +0530

    almost all divisors problem solved

--- maths/allmost-all-divisors.cpp
+++ maths/allmost-all-divisors.cpp
@@ -15,40 +15,24 @@ void solve(){
     sort(arr.begin(),arr.end());
     ll ans=(arr[0]*arr[n-1]);
     vector<ll>factors;
+    int ind=0;
     for(ll i=2;i*i<=ans;i++){
         if(ans%i==0){
-            // cout<<"pussing i= "<<i<<endl;
-            factors.push_back(i);
-            ll others=ans/i;
-            //cout<<"f= "<<i<<" ";
-            if(i != others){
-                //cout<<"f= "<<n/i<<" ";
-                // cout<<"pussing others= "<<others<<endl;
-                factors.push_back(others);
-            }
+           factors.push_back(i);
+           if(i != (ans/i)){
+               factors.push_back(ans/i);
+           }
+           
         }
     }
-    // cout<<"ans = "<<ans<<endl;
-    // cout<<"factors are "<<endl;
-    // for(int i=0;i<factors.size();i++){
-    //     cout<<factors[i]<<" ";
-    // }
-    // cout<<endl;
-    if(arr.size()==factors.size()){
-        sort(factors.begin(),factors.end());
-         for(ll i=0;i<arr.size();i++){
-            if(arr[i] != factors[i]){
-                ans=-1;
-                break;
-            }
-         }
-         cout<<ans<<endl;
-         return;
-    }else{
-        ans=-1;
-        cout<<ans<<endl;
-        return;
-    }
+   
+   sort(factors.begin(),factors.end());
+   if(factors==arr){
+       cout<<ans<<endl;
+       return;
+   }
+   cout<<"-1"<<endl;
+   return;
 }
 int main() {
     ios_base::sync_with_stdio(false);
