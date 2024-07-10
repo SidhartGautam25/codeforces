934ae598eff800fe6bf0bd7de4e8764dc6302796
sidhart25 <navneetnishchal1420@gmail.com>
2024-07-10 15:41:00 +0530

    c problem solved round 950

--- pracontest/d3r950/c.cpp
+++ pracontest/d3r950/c.cpp
@@ -3,33 +3,7 @@
 using namespace std;
 
 void solve(int tt){
-    if(tt==231){
-        int n;
-        cin>>n;
-        cout<<n<<endl;
-        vector<int>arr(n);
-        vector<int>b(n);
-        for(int i=0;i<n;i++){
-            cin>>arr[i];
-            cout<<arr[i]<<"  ";
-        }
-        cout<<endl;
-         for(int i=0;i<n;i++){
-            cin>>b[i];
-            cout<<b[i]<<"  ";
-        }
-        cout<<endl;
-        int m;
-        cin>>m;
-        cout<<m<<endl;
-        for(int i=0;i<m;i++){
-            int x;
-            cin>>x;
-            cout<<x<<"  ";
-        }
-        cout<<endl;
-
-    }
+   
   int n;
   cin>>n;
   vector<int>arr(n);
@@ -45,58 +19,42 @@ void solve(int tt){
   vector<int>d(m);
   map<int,int>mp;
   map<int,int>count;
+  int last;
   for(int i=0;i<m;i++){
     cin>>d[i];
     mp[d[i]]=i;
     count[d[i]]++;
+    if(i==m-1){
+        last=d[i];
+    }
   }
-  vector<bool>ind(n,false);
   vector<bool>done(m,false);
-  int updated=0;
+  bool lastused=false;
   for(int i=0;i<n;i++){
+    if(b[i]==last){
+        lastused=true;
+
+    }
     if(arr[i]==b[i]){
-        ind[i]=true;
         done[mp[b[i]]]=true;
     }else{
         if(mp.find(b[i])==mp.end()){
             cout<<"NO"<<endl;
             return;
         }else{
-            ind[i]=true;
             done[mp[b[i]]]=true;
-            // cout<<"ith is done true "<<mp[b[i]]<<" b[i] is "<<b[i]<<endl;
             count[b[i]]--;
-            updated++;
+            
             if(count[b[i]]<0){
                 cout<<"NO"<<endl;
                 return;
             }
-           
-
         }
     }
-
   }
 
-//   for(auto it:count){
-//     if(it.second>0){
-//         if(updated>0){
-//             continue;
-//         }else{
-//             cout<<"NO"<<endl;
-//             return;
-//         }
-//     }
-//   }
-// cout<<"starts"<<endl;
-//  for(int i=0;i<m;i++){
-//      if(done[i]){
-//          cout<<"true"<<endl;
-//      }else{
-//          cout<<"false"<<endl;
-//      }
-//  }
-if(done[m-1]==false){
+
+if(!lastused){
     cout<<"NO"<<endl;
     return;
 }
